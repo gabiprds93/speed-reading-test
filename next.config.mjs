cae582365622a338c3c43e3b43854ab5ad8b8665
Gabriela Paredes <gabiprds.93@gmail.com>
2024-06-21 11:36:10 -0500

    Fix assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
-  basePath: "/speed-reading-test",
-  assetPrefix: '/speed-reading-test/',
+  assetPrefix: debug ? "" : "https://gabrielaparedes.com/speed-reading-test/",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
