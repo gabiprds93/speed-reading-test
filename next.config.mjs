2a15259cf2762a4f0e44f4d6b4230a74e4e55d13
Gabriela Paredes <gabiprds.93@gmail.com>
2024-06-21 12:45:30 -0500

    Delete config in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const debug = process.env.NODE_ENV !== "production";
-
 const nextConfig = {
-  basePath: debug ? "" : "https://gabrielaparedes.com/speed-reading-test",
-  assetPrefix: debug ? "" : "https://gabrielaparedes.com/speed-reading-test/",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
