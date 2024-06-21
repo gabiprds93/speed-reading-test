6bde98bf9ea19d8d382ab2e7a8538b78f0dd1032
Gabriela Paredes <gabiprds.93@gmail.com>
2024-06-21 11:39:57 -0500

    Fix assets

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
+  basePath: debug ? "" : "https://gabrielaparedes.com/speed-reading-test",
   assetPrefix: debug ? "" : "https://gabrielaparedes.com/speed-reading-test/",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
